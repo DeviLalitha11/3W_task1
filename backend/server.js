f19705280cf2a4ba9107a2fa70134a847d0b962d
DeviLalitha11 <157714634+DeviLalitha11@users.noreply.github.com>
2025-07-16 11:47:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,6 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://moonlit-heliotrope-e23ca5.netlify.app',
   'https://quiet-dolphin-5abdc0.netlify.app',
   'https://darling-biscotti-65a928.netlify.app/'
 ];
